0e7daa654b6f26c75302eb66f04bc1f07038a267
kevin <coderminer@163.com>
2022-04-18 21:53:11 +0800

    feat: add tailwindcss config

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <div class="h-screen w-screen bg-red-300"></div>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
