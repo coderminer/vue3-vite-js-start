8a046681cc065ba1e35236dad397d016abffcf15
kevin <coderminer@163.com>
2022-04-18 21:40:28 +0800

    feat: base verion

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    'vue/setup-compiler-macros': true
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
+  // add your custom rules here
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
